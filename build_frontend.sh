e6a31129acb298c789f646fd6a7b37addabcf604
Igor Vaiman <igor.vaiman@inten.to>
2023-07-21 17:18:58 +0400

    minor fix to build script

--- build_frontend.sh
+++ build_frontend.sh
@@ -6,9 +6,9 @@ set -e
 # echo "Building static frontend files from sources"
 # cd frontend && npm run build && cd ..
 
+rm -rf telebot_constructor/static/
 echo "Cleaning possible artifacts from previous build"
 mkdir -p telebot_constructor/static
-rm telebot_constructor/static/*
 
 echo "Copying frontend into package"
 cp frontend/public/* telebot_constructor/static*
