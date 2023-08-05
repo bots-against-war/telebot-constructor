4bd805626e3c7480d9ed66cf82850697edde01f9
ilia kri <iliakrast@gmail.com>
2023-08-05 21:07:50 +0200

    HACK23 | pr review fixies

--- telebot_constructor/bot_config.py
+++ telebot_constructor/bot_config.py
@@ -1,8 +1,20 @@
+from datetime import timedelta
 from typing import Optional
 
-import pydantic
+from pydantic import BaseModel
+from telebot_components.constants import times
+from telebot_components.feedback import FeedbackConfig
 
 
-class BotConfig(pydantic.BaseModel):
+class FeedbackHandlerConfig(BaseModel):
+    admin_chat_id: int
+    message_log_to_admin_chat: bool = True
+    force_category_selection: bool = False
+    hashtags_in_admin_chat: bool = True
+    hashtag_message_rarer_than: timedelta = times.FIVE_MINUTES
+
+
+class BotConfig(BaseModel):
     token_secret_name: str  # must correspond to a valid secret in secret store
-    admin_chat_id: Optional[int]
+    feedback_handler_config: Optional[FeedbackHandlerConfig] = None
+
--- telebot_constructor/construct.py
+++ telebot_constructor/construct.py
@@ -19,6 +19,7 @@ async def construct_bot(username: str, bot_name: str, bot_config: BotConfig, sec
     """Core bot construction function responsible for turning a config into a functional bot"""
     log_prefix = f"[{username}][{bot_name}] "
     bot_prefix = f"{username}-{bot_name}"
+    background_jobs = []
 
     logger.info(log_prefix + "Constructing bot")
 
@@ -38,8 +39,8 @@ async def construct_bot(username: str, bot_name: str, bot_config: BotConfig, sec
     # handlers setup (dummy)
 
     banned_store = BannedUsersStore(
-        redis,
-        bot_prefix,
+        redis=redis,
+        bot_prefix=bot_prefix,
         cached=False,
     )
 
@@ -48,43 +49,46 @@ async def construct_bot(username: str, bot_name: str, bot_config: BotConfig, sec
         await bot.reply_to(message, "hello world")
 
     # region Feedback
-    feedback_handler = FeedbackHandler(
-        admin_chat_id=bot_config.admin_chat_id,
-        redis=redis,
-        bot_prefix=bot_prefix,
-        config=FeedbackConfig(
-            message_log_to_admin_chat=True,
-            force_category_selection=False,
-            hashtags_in_admin_chat=True,
-            hashtag_message_rarer_than=times.FIVE_MINUTES,
-            unanswered_hashtag="unanswered",
-            confirm_forwarded_to_admin_rarer_than=times.FIVE_MINUTES,
-            user_anonymization=UserAnonymization.NONE,
-        ),
-        anti_spam=AntiSpam(
-            redis,
-            bot_prefix,
-            config=AntiSpamConfig(
-                throttle_after_messages=3,
-                throttle_duration=times.FIVE_MINUTES,
-                soft_ban_after_throttle_violations=5,
-                soft_ban_duration=times.HOUR,
+    feedback_config = bot_config.feedback_handler_config
+    if feedback_config:
+        feedback_handler = FeedbackHandler(
+            admin_chat_id=feedback_config.admin_chat_id,
+            redis=redis,
+            bot_prefix=bot_prefix,
+            config=FeedbackConfig(
+                message_log_to_admin_chat=feedback_config.message_log_to_admin_chat,
+                force_category_selection=feedback_config.force_category_selection,
+                hashtags_in_admin_chat=feedback_config.hashtags_in_admin_chat,
+                hashtag_message_rarer_than=feedback_config.hashtag_message_rarer_than,
+                unanswered_hashtag="unanswered",
+                confirm_forwarded_to_admin_rarer_than=times.FIVE_MINUTES,
+                user_anonymization=UserAnonymization.NONE,
             ),
-        ),
-        service_messages=ServiceMessages(
-            forwarded_to_admin_ok="Forwarded!",
-            you_must_select_category="Please select category first!",
-            throttling_template="⚠ Please send no more than {} messages in {}.",
-            copied_to_user_ok="Copied to user chat ✨",
-            can_not_delete_message="Unable to delete message.",
-            deleted_message_ok="Message successfully deleted!",
-        ),
-    )
-    await feedback_handler.setup(bot)
+            anti_spam=AntiSpam(
+                redis,
+                bot_prefix,
+                config=AntiSpamConfig(
+                    throttle_after_messages=3,
+                    throttle_duration=times.FIVE_MINUTES,
+                    soft_ban_after_throttle_violations=5,
+                    soft_ban_duration=times.HOUR,
+                ),
+            ),
+            service_messages=ServiceMessages(
+                forwarded_to_admin_ok="Forwarded!",
+                you_must_select_category="Please select category first!",
+                throttling_template="⚠ Please send no more than {} messages in {}.",
+                copied_to_user_ok="Copied to user chat ✨",
+                can_not_delete_message="Unable to delete message.",
+                deleted_message_ok="Message successfully deleted!",
+            ),
+        )
+        await feedback_handler.setup(bot)
+        background_jobs.extend(feedback_handler.background_jobs(base_url=None, server_listening_future=None))
     # endregion
 
     return BotRunner(
         bot_prefix=bot_prefix,
         bot=bot,
-        background_jobs=feedback_handler.background_jobs(base_url=None, server_listening_future=None),
+        background_jobs=background_jobs,
     )
