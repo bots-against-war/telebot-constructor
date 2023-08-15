7a971ca2b1e43e1d3d4f18e0f6d0b793be6891af
Igor Vaiman <gosha.vaiman@gmail.com>
2023-08-15 14:55:50 +0300

    flake fix

--- telebot_constructor/bot_config.py
+++ telebot_constructor/bot_config.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from typing import Any, Optional
+from typing import Optional
 
 from pydantic import BaseModel, model_validator
 from telebot_components.constants import times
