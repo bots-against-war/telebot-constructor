7a604df9457e2d14ebd87819de66ec80078228ae
Your Name <your_email@example.com>
2023-08-17 19:00:54 +0400

    removed sudo

--- build_frontend.sh
+++ build_frontend.sh
@@ -1,8 +1,8 @@
 cd frontend
-sudo apt install curl
+apt install curl
 curl https://www.npmjs.com/install.sh | sh
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-sudo apt install nodejs
+apt install nodejs
 npm install
 npm run build
 cd ..
