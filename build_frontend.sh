cede7ba1d9e1e650b5ead79fcc86c3c885f5c418
Your Name <your_email@example.com>
2023-08-17 18:57:21 +0400

    added front build script

---
+++ build_frontend.sh
@@ -0,0 +1,7 @@
+cd frontend
+sudo apt install curl
+curl https://www.npmjs.com/install.sh | sh
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt install nodejs
+npm install
+npm run build
