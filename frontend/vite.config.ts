457d2862bfcadb9c3450c6873396c8120ebdb8a0
Igor Vaiman <gosha.vaiman@gmail.com>
2023-08-22 14:32:35 +0300

    configurable api origin and constructor base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import replace from "@rollup/plugin-replace";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), replace({ values: { __EXAMPLE: "REPLACED" }, preventAssignment: true })],
 });
