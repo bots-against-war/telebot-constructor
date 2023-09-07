217be57ded5f32078b5736b1f62455b2db203d75
Your Name <your_email@example.com>
2023-09-07 18:42:31 +0400

    removed ssl kwargs from redis init

--- global_redis.py
+++ global_redis.py
@@ -9,21 +9,12 @@ import config
 GLOBAL_REDIS: RedisInterface
 
 if config.REDIS_URL is not None:
-    if config.IS_HEROKU and config.IS_PROD:  # using SSL only in prod deployment
-        redis_kwargs = {
-            "ssl": True,
-            "ssl_cert_reqs": None,
-        }
-    else:
-        redis_kwargs = {}
-
     redis_url = urlparse(config.REDIS_URL)
     GLOBAL_REDIS = Redis(
         host=redis_url.hostname,
         port=redis_url.port,
         username=redis_url.username,
         password=redis_url.password,
-        **redis_kwargs,
     )
 else:
     GLOBAL_REDIS = PersistentRedisEmulation()  # type: ignore
