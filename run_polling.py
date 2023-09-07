99e114a79a2b1892156a877a0444ec322cfaf795
Your Name <your_email@example.com>
2023-09-07 20:25:26 +0400

    refactored configs

--- run_polling.py
+++ run_polling.py
@@ -2,12 +2,13 @@ import asyncio
 import logging
 import os
 from pathlib import Path
+from urllib.parse import urlparse
 
+from redis.asyncio import Redis  # type: ignore
 from telebot_components.redis_utils.emulation import PersistentRedisEmulation
-from telebot_components.utils.secrets import TomlFileSecretStore
+from telebot_components.redis_utils.interface import RedisInterface
+from telebot_components.utils.secrets import RedisSecretStore
 
-from global_redis import GLOBAL_REDIS, cleanup_redis
-from secret_store import secret_store
 from telebot_constructor.app import TelebotConstructorApp
 from telebot_constructor.auth import NoAuth
 
@@ -15,7 +16,24 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 async def main() -> None:
-    redis = GLOBAL_REDIS
+    redis_url = urlparse(os.environ.get("REDIS_URL"))
+    redis = Redis(
+        host=redis_url.hostname,
+        port=redis_url.port,
+        username=redis_url.username,
+        password=redis_url.password,
+    )
+    # redis = PersistentRedisEmulation()  # type: ignore
+
+    secret_store = RedisSecretStore(
+        redis=redis,
+        encryption_key=str(os.environ.get("SECRETS_ENCRYPTION_KEY")),
+        secret_max_len=10 * 1024,
+        secrets_per_user=10,
+        scope_secrets_to_user=False,
+    )
+    # secret_store = TomlFileSecretStore(path=Path(__file__).parent.parent / "secrets.toml")
+
     app = TelebotConstructorApp(
         redis=redis,
         auth=NoAuth(),
