2ae32b1d3513d05a4b20237c3efd6c3fce3948b4
Igor Vaiman <gosha.vaiman@gmail.com>
2023-09-18 18:54:53 +0300

    local mode in run polling py

--- run_polling.py
+++ run_polling.py
@@ -5,10 +5,9 @@ from pathlib import Path
 from urllib.parse import urlparse
 
 from redis.asyncio import Redis  # type: ignore
-
-# from telebot_components.redis_utils.emulation import PersistentRedisEmulation
-# from telebot_components.redis_utils.interface import RedisInterface
-from telebot_components.utils.secrets import RedisSecretStore
+from telebot_components.redis_utils.emulation import PersistentRedisEmulation
+from telebot_components.redis_utils.interface import RedisInterface
+from telebot_components.utils.secrets import RedisSecretStore, TomlFileSecretStore, SecretStore
 
 from telebot_constructor.app import TelebotConstructorApp
 from telebot_constructor.auth import NoAuth
@@ -17,23 +16,29 @@ logging.basicConfig(level=logging.INFO)
 
 
 async def main() -> None:
-    redis_url = urlparse(os.environ.get("REDIS_URL"))
-    redis = Redis(
-        host=redis_url.hostname,
-        port=redis_url.port,
-        username=redis_url.username,
-        password=redis_url.password,
-    )
-    # redis = PersistentRedisEmulation()  # type: ignore
+    is_local = bool(os.environ.get("LOCAL"))
 
-    secret_store = RedisSecretStore(
-        redis=redis,
-        encryption_key=str(os.environ.get("SECRETS_ENCRYPTION_KEY")),
-        secret_max_len=10 * 1024,
-        secrets_per_user=10,
-        scope_secrets_to_user=False,
-    )
-    # secret_store = TomlFileSecretStore(path=Path(__file__).parent.parent / "secrets.toml")
+    if is_local:
+        redis = PersistentRedisEmulation()  # type: ignore
+    else:
+        redis_url = urlparse(os.environ.get("REDIS_URL"))
+        redis = Redis(
+            host=redis_url.hostname,
+            port=redis_url.port,
+            username=redis_url.username,
+            password=redis_url.password,
+        )
+
+    if is_local:
+        secret_store: SecretStore = TomlFileSecretStore(path=Path(__file__).parent.parent / "secrets.toml")
+    else:
+        secret_store = RedisSecretStore(
+            redis=redis,
+            encryption_key=str(os.environ.get("SECRETS_ENCRYPTION_KEY")),
+            secret_max_len=10 * 1024,
+            secrets_per_user=10,
+            scope_secrets_to_user=False,
+        )
 
     app = TelebotConstructorApp(
         redis=redis,
