2a54b0ba929aad4ba4b8ec79bfb64fe80afc9624
Igor Vaiman <gosha.vaiman@gmail.com>
2023-09-21 17:04:46 +0300

    update type utils to include new blocks and entrypoints

--- frontend/src/api/typeUtils.ts
+++ frontend/src/api/typeUtils.ts
@@ -5,6 +5,10 @@ export function getBlockId(c: UserFlowBlockConfig): string {
     return c.message.block_id;
   } else if (c.human_operator !== null) {
     return c.human_operator.block_id;
+  } else if (c.menu !== null) {
+    return c.menu.block_id;
+  } else if (c.form !== null) {
+    return c.form.block_id;
   } else {
     throw new Error(`getBlockId is outdated, unexpected config variant: ${c}`);
   }
@@ -13,6 +17,10 @@ export function getBlockId(c: UserFlowBlockConfig): string {
 export function getEntrypointId(c: UserFlowEntryPointConfig): string {
   if (c.command !== null) {
     return c.command.entrypoint_id;
+  } else if (c.catch_all !== null) {
+    return c.catch_all.entrypoint_id;
+  } else if (c.regex !== null) {
+    return c.regex.entrypoint_id;
   } else {
     throw new Error(`getEntrypointId is outdated, unexpected config variant: ${c}`);
   }
