411e6e7a80839e40051a04b3b9b67d4cd1134080
Slava Zakharov <slava.zakharov@mailbox.org>
2024-02-27 00:30:03 +0400

    Fix label default with `undefined` assignment

--- frontend/src/components/inputs/Textarea.svelte
+++ frontend/src/components/inputs/Textarea.svelte
@@ -3,7 +3,7 @@
   import InputWrapper from "./InputWrapper.svelte";
 
   export let value: string;
-  export let label: string | undefined;
+  export let label: string | undefined = undefined;
   export let required: boolean = true;
   export let placeholder: string = "";
   export let description: string | null = null;
