37bfa035edfae5745ff6df46dd94d01175e9f289
Slava Zakharov <slava.zakharov@mailbox.org>
2024-02-28 02:11:54 +0400

    Refactor common logic into a separate utils file

--- frontend/src/components/inputs/TextInput.svelte
+++ frontend/src/components/inputs/TextInput.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Input } from "flowbite-svelte";
   import InputWrapper from "./InputWrapper.svelte";
+  import * as utils from "./utils";
 
   export let value: string;
   export let styleClass: string = "";
@@ -11,32 +12,9 @@
   export let error: string | boolean | null = null;
   export let disabled: boolean = false;
   export let maxLength: number | null = null;
-  let name: string = defineName(label);
+  let name: string = utils.defineName(label);
 
-  function defineName(label: string | null): string {
-    // TODO move to utils
-    function makeUnique(value: string): string {
-      const serializedValue = value.toLocaleLowerCase().replaceAll(" ", "-");
-      return addCode(serializedValue);
-    }
-
-    function addCode(value: string): string {
-      const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-      let code = "-";
-      for (let i = 0; i < 7; ++i) {
-        const randomIndex = Math.floor(Math.random() * symbols.length);
-        code += symbols.slice(randomIndex, randomIndex + 1);
-      }
-
-      return value + code;
-    }
-
-    if (label) return makeUnique(label);
-
-    return makeUnique("implicit-input");
-  }
-
-  $: localError = maxLength && value.length > maxLength ? `Текст должен быть короче ${maxLength} символов` : null; // TODO move to utils, prevent code repetition
+  $: localError = utils.getLengthError(maxLength, value.length);
 </script>
 
 <InputWrapper {label} {description} error={localError || error} {required}>
