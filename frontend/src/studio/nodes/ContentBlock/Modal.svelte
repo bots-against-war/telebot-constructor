a03c1dccf7de7a396a350c4476d30329b9b66757
Slava Zakharov <slava.zakharov@mailbox.org>
2024-04-19 17:03:49 +0400

    Add input for the attachments
    - Encode attachments to base64 and write them to a config
    - When mounted component checks config for the available attachments, override them if user selects new ones, store existing ones otherwise

--- frontend/src/studio/nodes/ContentBlock/Modal.svelte
+++ frontend/src/studio/nodes/ContentBlock/Modal.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-  import type { ContentBlock } from "../../../api/types";
+  import { onMount } from "svelte";
+  import type { Attachments, ContentBlock, ContentBlockContentAttachment, Image } from "../../../api/types";
   import LocalizableTextInput from "../../components/LocalizableTextInput.svelte";
+  import { Fileupload, Label, Listgroup, ListgroupItem } from "flowbite-svelte";
   import NodeModalBody from "../../components/NodeModalBody.svelte";
   import NodeModalControls from "../../components/NodeModalControls.svelte";
   import { NODE_TITLE } from "../display";
@@ -8,16 +10,89 @@
   export let config: ContentBlock;
   export let onConfigUpdate: (newConfig: ContentBlock) => any;
 
-  function updateConfig() {
-    config.contents = [{ text: { text: editedMessageText, markup: "none" }, attachments: [] }];
+  let editedMessageText = config.contents[0].text?.text || "";
+  let files: FileList;
+
+  async function updateConfig(): Promise<void> {
+    const attachments = await serializeAttachments();
+    config.contents = [{ text: { text: editedMessageText, markup: "none" }, attachments }];
     onConfigUpdate(config);
   }
 
-  let editedMessageText = config.contents[0].text?.text || "";
+  async function serializeAttachments(): Promise<Attachments> {
+    const attachments: Attachments = [];
+
+    if (files.length === 0) return attachments;
+
+    // TODO Replace string for multiple files once backend is ready
+    // for (let i = 0; i < files.length; ++i) {
+    for (let i = 0; i < 1; ++i) {
+      const file = files[i];
+
+      try {
+        const image = await readFile(file);
+        const name = file.name;
+        attachments.push({ image, name });
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    return attachments;
+  }
+
+  async function readFile(file: File): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      let reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = () => reject(reader.error);
+
+      reader.readAsDataURL(file);
+    });
+  }
+
+  function decodeBase64(base64: string, name: string) {
+    const [type, data] = base64.split(";base64,");
+    const binary = atob(data);
+    const uint8 = new Uint8Array(binary.length);
+    for (let i = 0; i < binary.length; ++i) {
+      uint8[i] = binary.charCodeAt(i);
+    }
+
+    const file = new File([uint8], name, { type });
+    return file;
+  }
+
+  onMount(() => {
+    const attachments = config.contents[0].attachments;
+    if (!attachments.length) return;
+
+    const dataTransfer = new DataTransfer();
+    attachments.forEach((obj) => {
+      let { image, name } = obj;
+
+      if (typeof image !== "string") return;
+
+      const file = decodeBase64(image, name);
+      dataTransfer.items.add(file);
+    });
+    files = dataTransfer.files;
+  });
 </script>
 
 <NodeModalBody title={NODE_TITLE.content}>
   <LocalizableTextInput label="Текст сообщения" bind:value={editedMessageText} />
+
+  <Label class="pb-2" for="multiple_files">Upload multiple files</Label>
+  <Fileupload id="multiple_files" multiple bind:files />
+  <Listgroup items={files} let:item class="mt-2">
+    {#if item}
+      {item.name}
+    {:else}
+      <ListgroupItem>No files</ListgroupItem>
+    {/if}
+  </Listgroup>
+
   <NodeModalControls on:save={updateConfig} />
 </NodeModalBody>
 
