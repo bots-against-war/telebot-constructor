da26dfa8bcde096d6dfb735bd55a508b37c2b1a0
Igor Vaiman <gosha.vaiman@gmail.com>
2024-04-26 10:04:08 +0200

    editable form field name with default = prompt

--- frontend/src/studio/nodes/FormBlock/components/BaseFormFieldInputs.svelte
+++ frontend/src/studio/nodes/FormBlock/components/BaseFormFieldInputs.svelte
@@ -1,18 +1,53 @@
 <script lang="ts">
   import { Toggle } from "flowbite-svelte";
   import type { BaseFormFieldConfig } from "../../../../api/types";
+  import TextInput from "../../../../components/inputs/TextInput.svelte";
   import LocalizableTextInput from "../../../components/LocalizableTextInput.svelte";
   import { getRandomContent as getRandomFormExampleContent } from "../content";
-
+  import { Input } from "flowbite-svelte";
+  import ActionIcon from "../../../../components/ActionIcon.svelte";
+  import { CheckOutline, PenOutline } from "flowbite-svelte-icons";
+  import { localizableTextToString } from "../../../utils";
+  import { languageConfigStore } from "../../../stores";
   export let config: BaseFormFieldConfig;
 
   let exampleContent = getRandomFormExampleContent();
-
-  config.name = "name";
+  let hasEditedFieldName = config.name !== localizableTextToString(config.prompt, $languageConfigStore);
+  let isEditingFieldName = false;
+  $: {
+    if (!hasEditedFieldName) {
+      config.name = localizableTextToString(config.prompt, $languageConfigStore);
+    }
+  }
 </script>
 
 <div class="flex flex-col gap-1">
-  <!-- <TextInput label="Поле" placeholder={exampleContent.name} bind:value={config.name} /> -->
-  <LocalizableTextInput label="Вопрос" placeholder={exampleContent.prompt} bind:value={config.prompt} />
-  <Toggle bind:checked={config.is_required} size="default">Обязательное</Toggle>
+  {#if config.name}
+    {#if isEditingFieldName}
+      <div class="flex flex-row gap-3 items-baseline">
+        <Input class="font-semibold py-1" placeholder={exampleContent.name} bind:value={config.name} />
+        <ActionIcon
+          icon={CheckOutline}
+          on:click={() => {
+            isEditingFieldName = false;
+          }}
+        />
+      </div>
+    {:else}
+      <button
+        class="font-semibold cursor-text hover:underline p-0 border-none m-0 w-full text-left"
+        on:click={() => {
+          isEditingFieldName = true;
+          hasEditedFieldName = true;
+        }}
+      >
+        {#if config.is_required}
+          <span class="text-red-700">*</span>
+        {/if}
+        {config.name}
+      </button>
+    {/if}
+  {/if}
+  <LocalizableTextInput placeholder={exampleContent.prompt} bind:value={config.prompt} />
+  <Toggle bind:checked={config.is_required} size="small">Обязательное</Toggle>
 </div>
