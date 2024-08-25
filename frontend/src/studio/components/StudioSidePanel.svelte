3cb53bf2450fc899b4ce839726b95f5ae5cecef5
Igor Vaiman <igor.vaiman@inten.to>
2024-08-25 18:03:07 +0200

    MAKE STUFF SQUARE

--- frontend/src/studio/components/StudioSidePanel.svelte
+++ frontend/src/studio/components/StudioSidePanel.svelte
@@ -7,7 +7,7 @@
     position: fixed;
     top: 100px; /* TODO: unhardcode header height */
     left: 10px;
-    border-radius: 15px;
+    border-radius: 0;
     /* copied from svelvet controls css */
     box-shadow: var(--controls-shadow, var(--default-controls-shadow));
     border: solid 1px var(--controls-border, var(--default-controls-border));
