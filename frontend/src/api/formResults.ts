199020f7daf8af6f257e924f32005ac3323fe414
Igor Vaiman <igor.vaiman@inten.to>
2024-09-29 17:26:14 +0200

    frontend api method to download CSV added

--- frontend/src/api/formResults.ts
+++ frontend/src/api/formResults.ts
@@ -1,4 +1,4 @@
-import { toDataResult, toTrivialResult, type Result } from "../utils";
+import { err, ok, toDataResult, toTrivialResult, type Result } from "../utils";
 import { apiUrl } from "./config";
 import type { FormResultsPage } from "./types";
 import { paginationQuery } from "./utils";
@@ -23,4 +23,26 @@ export async function updateFormTitle(botId: string, formBlockId: string, newTit
     body: newTitle,
   });
   return await toTrivialResult(resp);
+}
+
+export async function exportFormResults(
+  botId: string,
+  formBlockId: string,
+  minDate: Date | null,
+  maxDate: Date | null,
+): Promise<Result<string>> {
+  const toTimestamp = (d: Date) => Math.floor(+d / 1000);
+
+  let queryParts = [];
+  if (minDate !== null) {
+    queryParts.push(`min_timestamp=${toTimestamp(minDate)}`);
+  }
+  if (maxDate !== null) {
+    queryParts.push(`max_timestamp=${toTimestamp(maxDate)}`);
+  }
+  const query = queryParts.length > 0 ? "?" + queryParts.join("&") : "";
+  const resp = await fetch(apiUrl(`/forms/${encode(botId)}/${encode(formBlockId)}/export${query}`));
+  const respText = await resp.text();
+  if (resp.ok) return ok(respText);
+  else return err(respText);
 }
