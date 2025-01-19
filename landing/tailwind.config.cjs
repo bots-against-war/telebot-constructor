a63effd504afe3af386be8a3509c5ae3f94ad00c
Igor Vaiman <igor.vaiman@inten.to>
2025-01-19 22:48:03 +0100

    unified theme for frontend and landing

--- landing/tailwind.config.cjs
+++ landing/tailwind.config.cjs
@@ -1,3 +1,4 @@
+import flowbiteThemeExtend from "../flowbite-theme-extend";
 /** @type {import('tailwindcss').Config}*/
 const config = {
   content: [
@@ -8,33 +9,7 @@ const config = {
   plugins: [require("flowbite/plugin")],
   darkMode: "class",
   theme: {
-    extend: {
-      colors: {
-        // zinc
-        primary: {
-          50: "#fafafa",
-          100: "#f4f4f5",
-          200: "#e4e4e7",
-          300: "#d4d4d8",
-          400: "#a1a1aa",
-          500: "#71717a",
-          600: "#52525b",
-          700: "#3f3f46",
-          800: "#27272a",
-          900: "#18181b",
-        },
-      },
-      borderRadius: {
-        none: "0",
-        sm: "0",
-        DEFAULT: "0",
-        md: "0",
-        lg: "0",
-        large: "0",
-        "2xl": "0",
-        full: "9999px",
-      },
-    },
+    extend: flowbiteThemeExtend,
   },
 };
 
